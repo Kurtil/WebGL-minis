ce980472477ffa8c0d08d7008bc5861450e45747
Gaëtan Lagier <gaetan.lagier@hotmail.fr>
2024-07-08 16:43:08 +0200

    update scene

--- 3D/RAY_TRACING/shaders/fragment/scene.js
+++ 3D/RAY_TRACING/shaders/fragment/scene.js
@@ -1,41 +1,104 @@
 export default `
 void TestSceneTrace(in vec3 rayPos, in vec3 rayDir, inout HitInfo hitInfo)
 {    
+    // to move the scene around, since we can't move the camera yet
+    vec3 sceneTranslation = vec3(0.0f, 0.0f, 0.0f);
+    vec4 sceneTranslation4 = vec4(sceneTranslation, 0.0f);
+    
+   	// back wall
     {
-        vec3 A = vec3(-15.0f, -15.0f, 22.0f);
-        vec3 B = vec3( 15.0f, -15.0f, 22.0f);
-        vec3 C = vec3( 15.0f,  15.0f, 22.0f);
-        vec3 D = vec3(-15.0f,  15.0f, 22.0f);
+        vec3 A = vec3(-12.6f, -12.6f, 25.0f) + sceneTranslation;
+        vec3 B = vec3( 12.6f, -12.6f, 25.0f) + sceneTranslation;
+        vec3 C = vec3( 12.6f,  12.6f, 25.0f) + sceneTranslation;
+        vec3 D = vec3(-12.6f,  12.6f, 25.0f) + sceneTranslation;
         if (TestQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
         {
             hitInfo.albedo = vec3(0.7f, 0.7f, 0.7f);
             hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
         }
-    }    
-     
-    if (TestSphereTrace(rayPos, rayDir, hitInfo, vec4(-10.0f, 0.0f, 20.0f, 1.0f)))
+	}    
+    
+    // floor
     {
-        hitInfo.albedo = vec3(1.0f, 0.1f, 0.1f);
+        vec3 A = vec3(-12.6f, -12.45f, 25.0f) + sceneTranslation;
+        vec3 B = vec3( 12.6f, -12.45f, 25.0f) + sceneTranslation;
+        vec3 C = vec3( 12.6f, -12.45f, 15.0f) + sceneTranslation;
+        vec3 D = vec3(-12.6f, -12.45f, 15.0f) + sceneTranslation;
+        if (TestQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
+        {
+            hitInfo.albedo = vec3(0.7f, 0.7f, 0.7f);
+            hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
+        }        
+    }
+    
+    // cieling
+    {
+        vec3 A = vec3(-12.6f, 12.5f, 25.0f) + sceneTranslation;
+        vec3 B = vec3( 12.6f, 12.5f, 25.0f) + sceneTranslation;
+        vec3 C = vec3( 12.6f, 12.5f, 15.0f) + sceneTranslation;
+        vec3 D = vec3(-12.6f, 12.5f, 15.0f) + sceneTranslation;
+        if (TestQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
+        {
+            hitInfo.albedo = vec3(0.7f, 0.7f, 0.7f);
+            hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
+        }        
+    }    
+    
+    // left wall
+    {
+        vec3 A = vec3(-12.5f, -12.6f, 25.0f) + sceneTranslation;
+        vec3 B = vec3(-12.5f, -12.6f, 15.0f) + sceneTranslation;
+        vec3 C = vec3(-12.5f,  12.6f, 15.0f) + sceneTranslation;
+        vec3 D = vec3(-12.5f,  12.6f, 25.0f) + sceneTranslation;
+        if (TestQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
+        {
+            hitInfo.albedo = vec3(0.7f, 0.1f, 0.1f);
+            hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
+        }        
+    }
+    
+    // right wall 
+    {
+        vec3 A = vec3( 12.5f, -12.6f, 25.0f) + sceneTranslation;
+        vec3 B = vec3( 12.5f, -12.6f, 15.0f) + sceneTranslation;
+        vec3 C = vec3( 12.5f,  12.6f, 15.0f) + sceneTranslation;
+        vec3 D = vec3( 12.5f,  12.6f, 25.0f) + sceneTranslation;
+        if (TestQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
+        {
+            hitInfo.albedo = vec3(0.1f, 0.7f, 0.1f);
+            hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
+        }        
+    }    
+    
+    // light
+    {
+        vec3 A = vec3(-5.0f, 12.4f,  22.5f) + sceneTranslation;
+        vec3 B = vec3( 5.0f, 12.4f,  22.5f) + sceneTranslation;
+        vec3 C = vec3( 5.0f, 12.4f,  17.5f) + sceneTranslation;
+        vec3 D = vec3(-5.0f, 12.4f,  17.5f) + sceneTranslation;
+        if (TestQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
+        {
+            hitInfo.albedo = vec3(0.0f, 0.0f, 0.0f);
+            hitInfo.emissive = vec3(1.0f, 0.9f, 0.7f) * 20.0f;
+        }        
+    }
+    
+	if (TestSphereTrace(rayPos, rayDir, hitInfo, vec4(-9.0f, -9.5f, 20.0f, 3.0f)+sceneTranslation4))
+    {
+        hitInfo.albedo = vec3(0.9f, 0.9f, 0.75f);
         hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);        
     } 
-     
-    if (TestSphereTrace(rayPos, rayDir, hitInfo, vec4(0.0f, 0.0f, 20.0f, 1.0f)))
+    
+	if (TestSphereTrace(rayPos, rayDir, hitInfo, vec4(0.0f, -9.5f, 20.0f, 3.0f)+sceneTranslation4))
     {
-        hitInfo.albedo = vec3(0.1f, 1.0f, 0.1f);
+        hitInfo.albedo = vec3(0.9f, 0.75f, 0.9f);
         hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);        
     }    
-     
-    if (TestSphereTrace(rayPos, rayDir, hitInfo, vec4(10.0f, 0.0f, 20.0f, 1.0f)))
+    
+	if (TestSphereTrace(rayPos, rayDir, hitInfo, vec4(9.0f, -9.5f, 20.0f, 3.0f)+sceneTranslation4))
     {
-        hitInfo.albedo = vec3(0.1f, 0.1f, 1.0f);
+        hitInfo.albedo = vec3(0.75f, 0.9f, 0.9f);
         hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
-    }           
-     
-     
-    if (TestSphereTrace(rayPos, rayDir, hitInfo, vec4(10.0f, 10.0f, 20.0f, 5.0f)))
-    {
-        hitInfo.albedo = vec3(0.0f, 0.0f, 0.0f);
-        hitInfo.emissive = vec3(1.0f, 0.9f, 0.7f) * 100.0f;
-    }         
+    }    
 }
 `;
