64a248c7f94421b439476eee4d64490eb65db289
Gaëtan Lagier <gaetan.lagier@hotmail.fr>
2024-07-11 15:51:46 +0200

    antialiasing

--- 3D/RAY_TRACING/shaders/fragment/main.js
+++ 3D/RAY_TRACING/shaders/fragment/main.js
@@ -11,8 +11,11 @@ const int SAMPLES_PER_FRAME = 10;
 
 void main()
 {
+    uint rngState = uint(uint(gl_FragCoord.x) * uint(1973) + uint(gl_FragCoord.y) * uint(9277) + uint(frame) * uint(26699)) | uint(1);
+
+    vec2 jitter = vec2(RandomFloat01(rngState), RandomFloat01(rngState)) - .5;
     // Normalized pixel coordinates (from -1 to 1)
-    vec2 uv = (gl_FragCoord.xy * 2.0 - resolution) / resolution.y;
+    vec2 uv = ((gl_FragCoord.xy + jitter) * 2.0 - resolution) / resolution.y;
 
     vec3 rayPosition = vec3(0);
 
@@ -24,17 +27,12 @@ void main()
 
     for (int i = 0; i < SAMPLES_PER_FRAME; i++) {
     // initialize a random number state based on frag coord and frame
-        uint rngState = uint(i + 1) * uint(uint(gl_FragCoord.x) * uint(1973) + uint(gl_FragCoord.y) * uint(9277) + uint(frame) * uint(26699)) | uint(1);
-
-        color += getRayColor(rayPosition, rayDir, rngState);
+        color += getRayColor(rayPosition, rayDir, rngState) / float(SAMPLES_PER_FRAME);
     }
-
-    color /= float(SAMPLES_PER_FRAME);
 
     vec2 texCoord = gl_FragCoord.xy / resolution;
     vec3 textureColor = texture(tex, texCoord).rgb;
 
     // Output to screen
     fragColor = vec4(mix(textureColor, color, 1. / (1. + frame)), 1.0);
-}`
-;
+}`;
