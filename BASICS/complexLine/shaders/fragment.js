d7be14e898156a40940b012437a6c6d6c79cf96c
Gaëtan Lagier <gaetan.lagier@hotmail.fr>
2024-07-26 10:07:03 +0200

    implement complex line shaders

--- BASICS/complexLine/shaders/fragment.js
+++ BASICS/complexLine/shaders/fragment.js
@@ -2,10 +2,42 @@ export default `
 precision highp float;
 
 flat in int v_index;
+
+in vec2 v_line_start;
+in vec2 v_line_end;
+
+const float small = .01;
+const float line_width = 0.1;
  
-out vec4 outColor; 
+out vec4 outColor;
+
+float distanceToLine(vec2 start, vec2 end, vec2 point) {
+  vec2 line = end - start;
+  vec2 pointToStart = point - start;
+  vec2 pointToEnd = point - end;
+  
+  float len = length(line);
+  float lengthToStart = dot(line, pointToStart) / len;
+  float lengthToEnd = dot(-line, pointToEnd) / len;
+  
+  if (lengthToStart < 0.0) {
+    return length(pointToStart);
+  } else if (lengthToEnd < 0.0) {
+    return length(pointToEnd);
+  } else {
+    return abs(dot(normalize(line), vec2(-pointToStart.y, pointToStart.x)));
+  }
+}
  
 void main() {
-   outColor = vec4(float(v_index) / 4., 0, 0, 1);
+  vec2 point = gl_FragCoord.xy / vec2(500) * 2.0 - 1.0;
+  float d = distanceToLine(v_line_start, v_line_end, point);
+
+  if (d < line_width) {
+    outColor = vec4(float(v_index) / 4., 0, 0, 1);
+  } else {
+    // anti-aliasing
+    outColor.a *= smoothstep(line_width, line_width + small, d);
+  }
 }
 `;
--- BASICS/complexLine/shaders/vertex.js
+++ BASICS/complexLine/shaders/vertex.js
@@ -2,6 +2,11 @@ export default `
 uniform sampler2D positionTexture;
 
 flat out int v_index;
+out vec2 v_line_start;
+out vec2 v_line_end;
+
+const float small = .01;
+const float line_width = 0.1;
 
 uniform mat3 matrix;
 
@@ -13,13 +18,17 @@ void main() {
   vec2 currentPosition = texelFetch(positionTexture, ivec2(pointIndex, 0), 0).rg;
   vec2 adjacentPosition = texelFetch(positionTexture, ivec2(pointIndex + 1, 0), 0).rg;
 
-  vec2 normal = normalize(vec2(currentPosition.y - adjacentPosition.y, adjacentPosition.x - currentPosition.x)) * 0.1;
+  vec2 normal = normalize(vec2(currentPosition.y - adjacentPosition.y, adjacentPosition.x - currentPosition.x)) * (line_width + small);
 
   normal *= pointIndex % 2 == 0 ? 1.0 : -1.0;
 
   vec2 position = currentPosition + (normal * (float(gl_VertexID % 2) * 2.0 - 1.0));
 
   vec2 worldPosition = (matrix * vec3(position, 1)).xy;
+
+  v_line_start = (matrix * vec3(currentPosition, 1)).xy;
+  v_line_end = (matrix * vec3(adjacentPosition, 1)).xy;
+
   gl_Position = vec4(worldPosition, 0, 1);
 }
 `;
