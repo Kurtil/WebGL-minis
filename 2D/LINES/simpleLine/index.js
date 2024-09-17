c76ef907d7721ae215858bf3bfd178405e06ebb7
Gaëtan Lagier <gaetan.lagier@hotmail.fr>
2024-09-17 11:13:07 +0200

    rename 2D > LINES + add simple line example

---
+++ 2D/LINES/simpleLine/index.js
@@ -0,0 +1,111 @@
+import { gl as GL, makeProgram, makeBuffer } from "webglutils";
+/**
+ * @type { WebGL2RenderingContext }
+ */
+const gl = GL;
+
+const LINE_WIDTH = 10;
+
+import vertexShaderSource from "./shaders/vertex.js";
+import fragmentShaderSource from "./shaders/fragment.js";
+
+const program = makeProgram(
+  vertexShaderSource,
+  fragmentShaderSource
+);
+
+gl.useProgram(program);
+
+/**
+ *            _________
+ *  (0, 0.5) |        /| (1, 0.5) 
+ *           |      /  |
+ *   (0, 0)  +    /    |
+ *           |  /      |
+ * (0, -0.5) |/________| (1, -0.5) 
+ */
+const segmentInstanceGeometry =  new Float32Array([
+  0, -0.5,
+  1, -0.5,
+  1,  0.5,
+  0, -0.5,
+  1,  0.5,
+  0,  0.5
+]);
+
+makeBuffer(segmentInstanceGeometry);
+const positionLocation = gl.getAttribLocation(program, "position");
+gl.enableVertexAttribArray(positionLocation);
+gl.vertexAttribPointer(
+  positionLocation,
+  2,
+  gl.FLOAT,
+  false,
+  0,
+  0,
+);
+gl.vertexAttribDivisor(positionLocation, 0);
+
+const points = new Float32Array([
+  // line 1
+  100, 100,
+  200, 200,
+  300, 100,
+
+  // line 2
+  400, 200,
+  300, 300,
+  400, 300,
+  100, 400,
+  // line 3 - TODO close stroke
+  400, 450,
+  450, 450,
+  450, 350,
+  // line 4
+  450, 300,
+  450, 200
+]);
+
+const pointsTexture = gl.createTexture();
+const pointsTextureNumber = 0;
+gl.activeTexture(gl.TEXTURE0 + pointsTextureNumber);
+gl.bindTexture(gl.TEXTURE_2D, pointsTexture);
+gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RG32F, points.length / 2, 1);
+gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, points.length / 2, 1, gl.RG, gl.FLOAT, points);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+// a 1 indexed array
+const segmentIndexes = new Uint32Array([
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+]);
+makeBuffer(segmentIndexes);
+const pointAIndexAttributeLocation = gl.getAttribLocation(program, "pointAIndex");
+gl.enableVertexAttribArray(pointAIndexAttributeLocation);
+gl.vertexAttribIPointer(pointAIndexAttributeLocation, 1, gl.UNSIGNED_INT, 0, 0);
+gl.vertexAttribDivisor(pointAIndexAttributeLocation, 1);
+
+const pointBIndexAttributeLocation = gl.getAttribLocation(program, "pointBIndex");
+gl.enableVertexAttribArray(pointBIndexAttributeLocation);
+gl.vertexAttribIPointer(pointBIndexAttributeLocation, 1, gl.UNSIGNED_INT, 0, Uint32Array.BYTES_PER_ELEMENT);
+gl.vertexAttribDivisor(pointBIndexAttributeLocation, 1);
+
+const resolutionLocation = gl.getUniformLocation(program, "resolution");
+const widthLocation = gl.getUniformLocation(program, "width");
+const pointsLocation = gl.getUniformLocation(program, "points");
+
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+const lineSegmentCount = segmentIndexes.length - 1;
+const instanceGeometryCount = segmentInstanceGeometry.length / 2;
+
+gl.uniform1i(pointsLocation, pointsTextureNumber);
+gl.uniform2f(resolutionLocation, gl.canvas.clientWidth, gl.canvas.clientHeight);
+gl.uniform1f(widthLocation, LINE_WIDTH);
+
+gl.drawArraysInstanced(
+  gl.TRIANGLES,
+  0,
+  instanceGeometryCount,
+  lineSegmentCount,
+);
---
+++ 2D/LINES/simpleLine/shaders/vertex.js
@@ -0,0 +1,29 @@
+export default `
+in vec2 position; // from the instance geometry
+in uint pointAIndex;
+in uint pointBIndex;
+
+uniform sampler2D points;
+
+uniform vec2 resolution;
+uniform float width;
+
+void main() {
+  float x1 = texelFetch(points, ivec2(pointAIndex - 1u, 0), 0).x;
+  float y1 = texelFetch(points, ivec2(pointAIndex - 1u, 0), 0).y;
+  float x2 = texelFetch(points, ivec2(pointBIndex - 1u, 0), 0).x;
+  float y2 = texelFetch(points, ivec2(pointBIndex - 1u, 0), 0).y;
+
+  vec2 pointA = vec2(x1, y1);
+  vec2 pointB = vec2(x2, y2);
+
+  vec2 segment = pointB - pointA;
+  vec2 normal = normalize(vec2(-segment.y, segment.x));
+
+  vec2 point = pointA + segment * position.x + normal * position.y * width;
+
+  vec2 clipPosition = (point * 2.0 - resolution) / resolution.y;
+
+  gl_Position = vec4(clipPosition, 0, 1);
+}
+`;
