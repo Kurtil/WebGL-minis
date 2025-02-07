ae8ecbbf26e0c259b1c8edde4838a11c630b229c
Gaëtan Lagier <gaetan.lagier@hotmail.fr>
2025-02-07 10:47:20 +0100

    add BASICS - Textures - sampler array

---
+++ BASICS/textures/samplerArray/index.js
@@ -0,0 +1,104 @@
+import { gl, makeProgram, makeBuffer } from "webglutils";
+
+const DATA_FROM_PIXEL_BUFFER = false;
+
+console.log("DATA_FROM_PIXEL_BUFFER : ", DATA_FROM_PIXEL_BUFFER);
+
+const vertexShaderSource = `
+in vec4 position;
+in vec2 texcoord;
+
+out vec2 v_texcoord;
+
+void main()
+{
+    gl_Position = position;
+    v_texcoord = texcoord;
+}`;
+
+const fragmentShaderSource = `
+precision mediump float;
+
+in vec2 v_texcoord;
+
+uniform sampler2D u_textures[4];
+
+out vec4 fragColor;
+
+void main()
+{
+    fragColor = texture(u_textures[3], v_texcoord);
+}`;
+
+const program = makeProgram(vertexShaderSource, fragmentShaderSource);
+
+const texturesLocation = gl.getUniformLocation(program, "u_textures");
+
+/*** TEXTURE ***/
+function generateRandomTextureData(size) {
+  const totalSize = size * size * 3;
+  const data = new Uint8Array(totalSize);
+  for (let i = 0; i < totalSize; i++) {
+    data[i] = Math.floor(Math.random() * 256);
+  }
+  return data;
+}
+
+const texture = gl.createTexture();
+const textureUnit = 6;
+gl.activeTexture(gl.TEXTURE0 + textureUnit);
+gl.bindTexture(gl.TEXTURE_2D, texture);
+
+const texuteDataSize = 5;
+
+// Test delay texture filling
+setTimeout(() => {
+  const textureData = generateRandomTextureData(texuteDataSize);
+
+  if (DATA_FROM_PIXEL_BUFFER) {
+    makeBuffer(textureData, gl.STATIC_DRAW, gl.PIXEL_UNPACK_BUFFER);
+  }
+
+  gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGB,
+    texuteDataSize,
+    texuteDataSize,
+    0,
+    gl.RGB,
+    gl.UNSIGNED_BYTE,
+    DATA_FROM_PIXEL_BUFFER ? 0 : textureData
+  );
+  gl.pixelStorei(gl.UNPACK_ALIGNMENT, 4); // Default value
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST); // Mandatory because default value use bitmap that are not generated here
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+}, 100);
+
+/*** POSITION ***/
+makeBuffer(new Float32Array([-0.5, -0.5, 0.5, -0.5, 0, 0.5]));
+
+const positionLocation = gl.getAttribLocation(program, "position");
+gl.enableVertexAttribArray(positionLocation);
+gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+makeBuffer(new Float32Array([0, 0, 1, 0, 0.5, 1]));
+
+/*** TEXTURE COORDINATE ***/
+const texcoordLocation = gl.getAttribLocation(program, "texcoord");
+gl.enableVertexAttribArray(texcoordLocation);
+gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+/*** RENDERING ***/
+gl.useProgram(program);
+
+gl.uniform1iv(texturesLocation, [, , ,textureUnit, , , , ]);
+
+const draw = () => gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+requestAnimationFrame(function drawLoop() {
+  draw();
+  requestAnimationFrame(drawLoop);
+});
